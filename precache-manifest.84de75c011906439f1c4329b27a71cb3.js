31548c0015ff0be3852f7224c0ea17de4761c1bc
alejandro <alejandrolivares9211@gmail.com>
2020-06-27 22:36:16 -0600

    Updates

---
+++ precache-manifest.84de75c011906439f1c4329b27a71cb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3112fc261312f06b02817eb35498e9ec",
+    "url": "/curriculum/index.html"
+  },
+  {
+    "revision": "6c7e321b777f374a63fb",
+    "url": "/curriculum/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0cbf4d73ebc9cbbb9302",
+    "url": "/curriculum/static/css/main.c8207260.chunk.css"
+  },
+  {
+    "revision": "6c7e321b777f374a63fb",
+    "url": "/curriculum/static/js/2.7e2a4bff.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/curriculum/static/js/2.7e2a4bff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cbf4d73ebc9cbbb9302",
+    "url": "/curriculum/static/js/main.df590641.chunk.js"
+  },
+  {
+    "revision": "ea8537b673971a69b88d",
+    "url": "/curriculum/static/js/runtime-main.6f6a4d16.js"
+  }
+]);
