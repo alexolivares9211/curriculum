ab07da446ea3a7db71e0fa85c7b0be4fddf6c4e7
alejandro <alejandrolivares9211@gmail.com>
2020-07-05 19:14:16 -0600

    align paragraph center

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -6,9 +6,10 @@ class AboutComponent extends Component {
         return (
             <Container fluid className="sections-container" id="about">
                 <Row noGutters xl={1}>
-                    <Col><h1 className="title-sections">¿Quien Soy?</h1></Col>
-                    <Col>
-                        <p>
+                    <Col className="col-1"><h1 className="title-sections">¿Quien Soy?</h1></Col>
+
+                    <Col className="col-2" xl="auto">
+                        <p className="pp">
                             Me considero una persona responsable, con iniciativa
                             y con facilidad para integrarme a un equipo de trabajo,
                             con liderazgo y honestidad para cumplir los objetivos
