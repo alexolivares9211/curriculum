6c9cdf08fbc2197a10db1d442ab0c008c1b93975
alejandro <alejandrolivares9211@gmail.com>
2020-07-10 11:27:51 -0600

    quien soy section change

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -9,11 +9,19 @@ class AboutComponent extends Component {
                     <Col className="col-1"><h1 className="title-sections">¿Quien Soy?</h1></Col>
 
                     <Col className="col-2" xl="auto">
-                        <p className="pp">
+                        {/* <p className="pp">
                             Me considero una persona responsable, con iniciativa
                             y con facilidad para integrarme a un equipo de trabajo,
                             con liderazgo y honestidad para cumplir los objetivos
                             asignados.
+                        </p> */}
+                        <p className="pp">
+                            Soy Ingeniero en Computación, con experiencia en desarrollo de software,
+                            tanto en frontend como en backend.
+                        </p>
+                        <p className="pp">
+                            Tengo el deseo de seguir superandome a mi mismo, aprendiendo nuevas tecnologías
+                            para aportar soluciones optimas e involucrarme en proyectos acorde a mi perfil.
                         </p>
                     </Col>
                 </Row>
